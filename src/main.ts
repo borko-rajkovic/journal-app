de39aaff7629121ae20b7b058be4e0dd10d849bd
Borko Rajkovic <rajkovicborko@gmail.com>
2019-08-16 00:28:03 +0200

    Nest.JS enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
 }
