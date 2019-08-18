a7a1f090350212672c154a43c23003c952a1f434
Borko Rajkovic <rajkovicborko@gmail.com>
2019-08-18 03:02:06 +0200

    LatestThreeNotes render conditionally depending on what data is available

--- client/components/LatestThreeNotes.tsx
+++ client/components/LatestThreeNotes.tsx
@@ -26,22 +26,26 @@ const LatestThreeNotesLoaded = ({ notes }: any) => (
           float={1}
         />
       </div>
-      <div className="col-4 d-flex align-items-stretch">
-        <Note
-          body={notes[1].body}
-          title={notes[1].title}
-          createdDate={notes[1].updatedDate}
-          float={2}
-        />
-      </div>
-      <div className="col-4 d-flex align-items-stretch">
-        <Note
-          body={notes[2].body}
-          title={notes[2].title}
-          createdDate={notes[2].updatedDate}
-          float={3}
-        />
-      </div>
+      {notes.length > 1 ? (
+        <div className="col-4 d-flex align-items-stretch">
+          <Note
+            body={notes[1].body}
+            title={notes[1].title}
+            createdDate={notes[1].updatedDate}
+            float={2}
+          />
+        </div>
+      ) : null}
+      {notes.length > 2 ? (
+        <div className="col-4 d-flex align-items-stretch">
+          <Note
+            body={notes[2].body}
+            title={notes[2].title}
+            createdDate={notes[2].updatedDate}
+            float={3}
+          />
+        </div>
+      ) : null}
     </div>
   </React.Fragment>
 );
@@ -50,7 +54,6 @@ const LatestThreeNotes = () => {
   return (
     <Query query={LATEST_THREE_NOTES}>
       {({ data, error, loading }: any) => {
-        console.log('data', data);
         const { notes } = data;
         return loading ? (
           <div className="text-center my-4">
@@ -60,8 +63,21 @@ const LatestThreeNotes = () => {
           <div className="alert alert-danger" role="alert">
             Something went wrong!
           </div>
-        ) : (
+        ) : notes.length > 0 ? (
           <LatestThreeNotesLoaded notes={notes} error={error} />
+        ) : (
+          <div className="alert alert-info" role="alert">
+            <h4 className="alert-heading">Make some notes!</h4>
+            <p>
+              You don't have any notes yet. We encourage you to try create some
+              new notes and play around with the application. Thank you for your
+              trust and time you spend to check out this application.
+            </p>
+            <hr />
+            <p className="mb-0">
+              This alert will be removed once you have at least one note.
+            </p>
+          </div>
         );
       }}
     </Query>
