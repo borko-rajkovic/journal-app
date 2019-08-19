2264c25a366d3a90f9c8dc46b2305f5d89bbcb1c
Borko Rajkovic <rajkovicborko@gmail.com>
2019-08-20 00:53:14 +0200

    Custom error pages

---
+++ client/pages/_error.tsx
@@ -0,0 +1,67 @@
+import Link from 'next/link';
+import { withRouter } from 'next/router';
+import React from 'react';
+import Layout from '../components/Layout';
+import App from 'next/app';
+
+class ErrorPage extends App {
+  static getInitialProps = async ({ res, xhr }: any): Promise<any> => {
+    const errorCode = res ? res.statusCode : xhr ? xhr.status : null;
+    return { errorCode };
+  };
+
+  render() {
+    console.log('props', this.props);
+    let response: any;
+    switch ((this.props as any).errorCode) {
+      case 200: // Also display a 404 if someone requests /_error explicitly
+      case 404:
+        response = (
+          <div>
+            <Layout>
+              <h1 className="display-4">Page Not Found</h1>
+              <p>
+                The page <strong>{(this.props as any).router.asPath}</strong>{' '}
+                does not exist.
+              </p>
+              <p>
+                <Link href="/">
+                  <a>Home</a>
+                </Link>
+              </p>
+            </Layout>
+          </div>
+        );
+        break;
+      case 500:
+        response = (
+          <div>
+            <Layout>
+              <h1 className="display-4">Internal Server Error</h1>
+              <p>An internal server error occurred.</p>
+            </Layout>
+          </div>
+        );
+        break;
+      default:
+        response = (
+          <div>
+            <Layout>
+              <h1 className="display-4">
+                HTTP {(this.props as any).errorCode} Error
+              </h1>
+              <p>
+                An <strong>HTTP {(this.props as any).errorCode}</strong> error
+                occurred while trying to access{' '}
+                <strong>{(this.props as any).router.pathname}</strong>
+              </p>
+            </Layout>
+          </div>
+        );
+    }
+
+    return response;
+  }
+}
+
+export default withRouter(ErrorPage);
