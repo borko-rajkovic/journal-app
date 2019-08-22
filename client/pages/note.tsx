2536bbdf9c4a9b9eac6697463f04619ce0e33d1e
Borko Rajkovic <rajkovicborko@gmail.com>
2019-08-22 23:54:40 +0200

    Note page

---
+++ client/pages/note.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+
+import Layout from '../components/Layout';
+import Note from '../components/Note';
+import { User } from '../interfaces';
+import checkLoggedIn from '../lib/checkLoggedIn';
+import redirect from '../lib/redirect';
+import { ApolloConsumer } from 'react-apollo';
+
+interface Props {
+  item?: User;
+  errors?: string;
+}
+
+class InitialPropsDetail extends React.Component<Props> {
+  static getInitialProps = async (context: any) => {
+    const { loggedInUser } = await checkLoggedIn(context.apolloClient);
+
+    if (!loggedInUser.whoAmI) {
+      // Throw them back to the main page
+      redirect(context, '/');
+    }
+
+    return { loggedInUser, query: context.query };
+  };
+
+  render() {
+    if (!(this.props as any).query.id) {
+      return (
+        <Layout>
+          <div className="alert alert-danger" role="alert">
+            Something went wrong!
+          </div>
+        </Layout>
+      );
+    }
+
+    const { id, edit } = (this.props as any).query;
+
+    return (
+      <ApolloConsumer>
+        {() => (
+          <Layout>
+            <Note id={id} edit={edit} />
+          </Layout>
+        )}
+      </ApolloConsumer>
+    );
+  }
+}
+
+export default InitialPropsDetail;
